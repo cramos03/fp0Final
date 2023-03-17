0a3127ac1ee6a17d02f015229a00d86c5144de38
cramos03 <cramos03@uw.edu>
2023-03-17 11:43:30 -0700

    new

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
-import {
-    BoxGeometry,
-    Mesh,
-    MeshPhongMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-    DirectionalLight,
-  } from "three";
+let pen;
+let button;
+let clearbutton;
+let slider;
+let colorPicker;
+let isSliderInteracted = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  button = createButton('Save Art');
+  button.position(40, windowHeight - 50);
+  button.mousePressed(saveArt);
+  button.size(85, 25);
+  button.style("font-family", "Helvetica");
+  button.style("font-size", "15px");
+
+  clearbutton = createButton('Clear Board');
+  clearbutton.position(25, windowHeight - 90);
+  clearbutton.mousePressed(clearBoard);
+  clearbutton.size(110, 25);
+  clearbutton.style("font-family", "Helvetica");
+  clearbutton.style("font-size", "15px");
   
-  // Create our scene
-  const scene = new Scene();
-  
-  // Create the camera so we can see our scene
-  const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  
-  // Create our renderer and add it to the DOM
-  const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-  
-  // Create our cube mesh from  a geometry and a material and add it to the scene
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshPhongMaterial({ color: 0x00ff00 });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
-  
-  // Add a directional light so we can see shadows on the cube
-  const color = 0xffffff;
-  const intensity = 1;
-  const light = new DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
-  
-  // Position the camera
-  camera.position.z = 5;
-  
-  // The animation loop updates the cube's rotation
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+  slider = createSlider(0.5, 50, 1);
+  slider.position(20, 125);
+  slider.style("width", "100px");
+
+  colorPicker = createColorPicker("#ffffff");
+  colorPicker.position(50, 75);
+}
+
+function draw() {
+  noStroke()
+  fill(200);
+  rect(0,0,150,windowHeight,0,20,20,0)
+  let weight = slider.value();
+
+  if (!isSliderInteracted && mouseIsPressed) {
+    stroke(pen);
+    strokeWeight(weight);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
+}
+
+function mousePressed() {
+    pen = colorPicker.color();
   
-  // Start the animation loop
-  animate();
+}
+
+function mouseDragged() {
+  if (mouseX >= slider.x && mouseX <= slider.x + slider.width && mouseY >= slider.y && mouseY <= slider.y + slider.height) {
+    isSliderInteracted = true;
+  }
+}
+
+function mouseReleased() {
+  isSliderInteracted = false;
+}
+
+function saveArt() {
+  saveCanvas('mycanvas',"png");
+}
+
+function clearBoard() {
+  background(255);
+}
