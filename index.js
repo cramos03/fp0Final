cceb412d77700ee72a95e7e95f3cb80c09b59b49
cramos03 <cramos03@uw.edu>
2023-03-17 18:00:17 -0700

    finalized

--- index.js
+++ index.js
@@ -3,37 +3,45 @@ let button;
 let clearbutton;
 let slider;
 let colorPicker;
+let menuWidth = 120
 let isSliderInteracted = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+  saveState();
   button = createButton('Save Art');
-  button.position(40, windowHeight - 50);
-  button.mousePressed(saveArt);
-  button.size(85, 25);
+  button.position(30, windowHeight - 50);
+  button.size(55, 20);
   button.style("font-family", "Helvetica");
-  button.style("font-size", "15px");
+  button.style("font-size", "10px");
 
+  button.mousePressed(function(){
+    let graphics = createGraphics(width-menuWidth, height);
+    graphics.image(get(menuWidth, 0, width-menuWidth, height), 0, 0);
+    saveCanvas(graphics,'myDrawing','png');
+    });
+  
   clearbutton = createButton('Clear Board');
-  clearbutton.position(25, windowHeight - 90);
+  clearbutton.position(23, windowHeight - 90);
   clearbutton.mousePressed(clearBoard);
-  clearbutton.size(110, 25);
+  clearbutton.size(70, 20);
   clearbutton.style("font-family", "Helvetica");
-  clearbutton.style("font-size", "15px");
+  clearbutton.style("font-size", "10px");
   
   slider = createSlider(0.5, 50, 1);
-  slider.position(20, 125);
+  slider.position(5, 125);
   slider.style("width", "100px");
 
   colorPicker = createColorPicker("#ffffff");
-  colorPicker.position(50, 75);
+  colorPicker.position(35, 75);
 }
 
 function draw() {
   noStroke()
   fill(200);
-  rect(0,0,150,windowHeight,0,20,20,0)
+  rect(0,0,120,windowHeight,0,20,20,0)
+  
   let weight = slider.value();
 
   if (!isSliderInteracted && mouseIsPressed) {
@@ -58,10 +66,10 @@ function mouseReleased() {
   isSliderInteracted = false;
 }
 
-function saveArt() {
-  saveCanvas('mycanvas',"png");
-}
-
 function clearBoard() {
   background(255);
+}
+
+function saveState(){
+  previousState = get();
 }
